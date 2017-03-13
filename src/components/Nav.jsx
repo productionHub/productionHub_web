ff186e51771ee47c30202c100afbca0029a9a725
Tim Chin <timm.chin@gmail.com>
2017-03-12 19:26:37 -0700

    Adds Nav file with basic <ul>

---
+++ src/components/Nav.jsx
@@ -0,0 +1,23 @@
+var Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        <a className="media-icons-container left-nav"><i className="fa fa-calendar"></i></a>
+      </li>
+      <li>
+        <a className="media-icons-container right-nav"><i className="fa fa-home"></i></a>
+      </li>
+      <li>
+        <a className="media-icons-container right-nav"><i className="fa fa-user"></i></a>
+      </li>
+      <li>
+        <a className="media-icons-container right-nav"><i className="fa fa-envelope"></i></a>
+      </li>
+      <li>
+        <a className="media-icons-container right-nav"><i className="fa fa-cog"></i></a>
+      </li>
+    </ul>
+  </nav>
+);
+
+window.Nav = Nav;
